a6b9d9aeb2dea15dc858ba37ef6e5fbba2617f7c
BekirYldrm <bekiryildirim.2508@gmail.com>
2023-06-28 23:32:55 +0300

    connected to the project database

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const app = express();
-
-let posts = [];
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const homeContent = " Hello, this project is a code exercise I did to improve myself. With this project, it is aimed to have information about database and back-end services. In this project, there are many features such as generating answers for get and post requests and using the project in interaction with the database.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+mongoose.connect("mongodb+srv://bekir_yildirim:25.08.2001Bekir@cluster0.z4l3wpv.mongodb.net/BlogDB");
+
+const postSchema = mongoose.Schema({ title: String, content: String });
+const postModel = mongoose.model("Post", postSchema);
+
 
 app.get("/", function (req, res) {
-    res.render("home", { homeContent: homeStartingContent , homePosts : posts });
+    postModel.find().then(docs => {
+        res.render("home", { homeContent: homeContent, posts: docs });
+    }).catch(err => { });
 
 });
+
 app.get("/about", function (req, res) {
     res.render("about", { aboutContent: aboutContent });
 });
@@ -28,42 +37,32 @@ app.get("/compose", function (req, res) {
     res.render("compose")
 });
 
+
 app.post("/compose", function (req, res) {
-    const post = {
+    const post = new postModel({
         title: req.body.composeInput,
         content: req.body.composeTextarea
-    }
-    posts.push(post);
-    res.redirect("/");
-
+    });
+    post.save().then(function () {
+        res.redirect("/");
+    }).catch(err => {
+        console.log(err);
+    });
 });
 
-app.get("/post/:postName" , function(req ,res){
-    const urlTitle=_.lowerCase(req.params.postName);
-    posts.forEach(function (element){
-        const postTitle = _.lowerCase(element.title);
-        if(urlTitle === postTitle){
-            res.render("post" , {postTitle : element.title  , postContent : element.content });
-        }
+
+
+app.get("/post/:postName", function (req, res) {
+    const urlTitle = _.lowerCase(req.params.postName);
+    postModel.find().then(posts => {
+        posts.forEach(function (post) {
+            const postTitle = _.lowerCase(post.title);
+            if (urlTitle === postTitle) {
+                res.render("post", { postTitle: post.title, postContent: post.content });
+            }
+        });
     });
-        
-    
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 app.listen(3000 || process.env.POST, function () {

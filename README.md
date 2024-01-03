3e70f671664aaacb90c42cd6c5a24f5b3a32783f
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-03 23:17:01 +0300

    Readme.md updated.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Blog_Project.
+# About the project
+You can create your own blog page by filling out the form in the compose section of this site.
+
+
+## Technologies and methods used in the project
+
+* This project aims to develop applications with Mongo Atlas, Express Route Routings, Get and Post events.
+
+* Dynamic web pages were created with the [EJS](https://ejs.co/) templating language.
+
+
+## Requirements to run the project
+
+```
+npm i express  
+npm i body-parser  
+npm i lodash
+npm i mongoose
+npm i mongodb
+```
+
+After installing these modules from the terminal, you can run the project.
--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app
     .use(bodyParser.urlencoded({ extended: true }))
     .use(express.static("public"));
 
-const homeContent = " Hello, this project is a code exercise I did to improve myself. With this project, it is aimed to have information about database and back-end services. In this project, there are many features such as generating answers for get and post requests and using the project in interaction with the database.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeContent = "Hello our user, You can create your own blog page by filling out the form in the compose section of this site. Our aim on this site is to be informed about the latest posts of bloggers that interest you and to create your own blog page";
+
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 
@@ -32,7 +32,7 @@ app
     })
 
     .get("/about", (req, res) => {
-        res.render("about", { aboutContent: aboutContent });
+        res.render("about");
     })
 
     .get("/contact", (req, res) => {
